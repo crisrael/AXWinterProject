1c208e02f9d9ec884c130231ee24a7564b9786b8
crisrael <cmlucero37@gmail.com>
2015-07-23 19:13:36 -0700

    Create site.master.cs

---
+++ site.master.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Only the admin account has access to the admin tools
+        if (HttpContext.Current.User.Identity.Name == "Admin")
+            adminToolsLink.Visible = true;
+    }
+
+    public void logoutLink_OnClick(object sender, EventArgs args)
+    {
+        //Sign the user out
+        FormsAuthentication.SignOut();
+        Response.Redirect(ResolveUrl("~/login.aspx"));
+    }
+}
